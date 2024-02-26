00b31db9c19e79d6d539faeec7558c65c525ae7b
Luke Waehner <luke.waehner@gmail.com>
2024-02-26 14:34:01 -0500

    work

--- Math2001/Homework/R01hw05.lean
+++ Math2001/Homework/R01hw05.lean
@@ -39,12 +39,25 @@ theorem problem6 (P Q : Prop) : (P ∨ Q) ↔ (Q ∨ P) := by
 @[autograded 5]
 theorem problem7 (P : α → Prop) (Q : Prop) : ((∃ x, P x) ∧ Q) ↔ ∃ x, (P x ∧ Q) := by
   constructor
-  intro b
-  obtain ⟨ b1, b2 ⟩ := b
+  intro ha
+  obtain ⟨ a1, a2 ⟩ := ha
+  obtain ⟨ a, haa ⟩ := a1
+  use a
+  constructor
+  apply haa
+  apply a2
 
+  intro hb
+  obtain ⟨ b1, b2 ⟩ := hb
+  obtain ⟨ bb1, bb2 ⟩ := b2
+  constructor
+  use b1
+  use bb1
+  use bb2
 
 
 @[autograded 4]
 theorem problem8 : (¬ ∀ a b : ℤ, a * b = 1 → a = 1 ∨ b = 1)
-    ↔ ∃ a b : ℤ, a * b = 1 ∧ a ≠ 1 ∧ b ≠ 1 :=
-  sorry
+    ↔ ∃ a b : ℤ, a * b = 1 ∧ a ≠ 1 ∧ b ≠ 1 := by
+  constructor
+  intro ha
