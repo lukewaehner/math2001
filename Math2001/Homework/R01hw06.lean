c2466ce2d871b33f214bc45e7be86915f4537582
Dave Swinarski <dswinarski@fordham.edu>
2024-03-14 05:45:39 -0400

    Update R01hw06.lean

--- Math2001/Homework/R01hw06.lean
+++ Math2001/Homework/R01hw06.lean
@@ -28,10 +28,11 @@ theorem problem1 : forall_sufficiently_large n : ℕ, (3:ℤ) ^ n ≥ 2 ^ n + 10
   dsimp
   sorry
 
-@[autograded 4]
+
 /-
-It is possible, perhaps even likely that you will get an error in the autograder for this problem, even if you get no errors in GitPod. You may ignore the autograder errors in Gradescope as long as it runs correctly in GitPod.
+It is possible, perhaps even likely, that you will get an error in the autograder for this problem, even if you get no errors in GitPod. You may ignore the autograder errors in Gradescope as long as it runs correctly in GitPod.
 -/ 
+@[autograded 4]
 theorem problem2 : 6*(Finset.sum (range (n + 1)) (fun i : ℕ ↦ i^2)) =  n * (n + 1)*(2*n+1) := by
   sorry
 
@@ -42,18 +43,19 @@ def b : ℕ → ℤ
   | 1 => 1
   | n + 2 => 5 * b (n + 1) - 6 * b n
 
-@[autograded 4]
+
 -- Do 3a with two step induction
+@[autograded 4]
 theorem problem3a (n : ℕ) : b n = 3 ^ n - 2 ^ n := by
   sorry
 
 
-@[autograded 4]
 -- Do 3b with strong induction
+@[autograded 4]
 theorem problem3b (n : ℕ) : b n = 3 ^ n - 2 ^ n := by
   sorry
 
-@[autograded 4]
+
 /- Hints:
 Apply the lemma even_or_odd n
 In the even case, prove the additional hypotheses that 0 is less than k
@@ -61,5 +63,6 @@ and k is less than n
 Then get an inductive hypothesis by calling problem4, as if we were doing
 strong induction
 -/
+@[autograded 4]
 theorem problem4 (n : ℕ) (hn : 0 < n) : ∃ a x, Odd x ∧ n = 2 ^ a * x := by
   sorry
